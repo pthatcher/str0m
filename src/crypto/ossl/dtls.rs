3e18edec8d31bc5f4e3c7aa7fe7ea72809d627bd
Eduardo Fernandez <edufernandez@microsoft.com>
2025-01-16 13:46:15 -0800

    again

--- src/crypto/ossl/dtls.rs
+++ src/crypto/ossl/dtls.rs
@@ -16,7 +16,7 @@ use super::stream::TlsStream;
 use super::CryptoError;
 
 const DTLS_CIPHERS: &str = "EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH";
-const DTLS_EC_CURVES: &str = "X25519:P-256:P-521:P-384:secp256k1";
+const DTLS_EC_CURVES: &str = "X25519:P-256:P-521:P-384";
 const DTLS_ECDH_CURVE: Nid = Nid::X9_62_PRIME256V1;
 
 pub struct OsslDtlsImpl {
